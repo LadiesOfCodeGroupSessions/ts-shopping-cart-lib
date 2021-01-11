d355cbdfe40b42a6c60cc85a5d8edea368754e72
Sarah Usher <sarahnusher@gmail.com>
2021-01-11 18:45:05 +0000

    working dictionary

--- src/main/Cart.ts
+++ src/main/Cart.ts
@@ -1,29 +1,31 @@
 import {Item} from './Item'
 
 export class Cart {
-  specials = new Map()
-
-    constructor()
-    {
-        this.specials.set("Apple", {quantity:3, price:130})
-        this.specials.set("Banana", {quantity:2, price:45})   
-    }
+    public specials = new Map()
 
     private items: Item[] = []
+
+    constructor() {
+        this.specials.set('Apple', {quantity: 3, price: 130})
+        this.specials.set('Banana', {quantity: 2, price: 45})
+    }
 
     public addItem(item: Item) {
         this.items.push(item)
     }
 
     public getTotal() {
-
         let total = 0
 
         this.items.forEach((item) => {
-            if (this.specials.has('Apple')) {
-                return this.specials.get('Apple').price
+            if (this.specials.has(item.name)) {
+                const special = this.specials.get(item.name)
+                if (special.quantity === item.quantity) {
+                    total += special.price
+                    item.quantity = 0
+                }
             }
-      })
+        })
 
         return total + this.items.reduce((acc, item) => acc + (item.price * item.quantity), 0)
     }
