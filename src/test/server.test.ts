a89a2c5847adafcc844fe0f8a386b9373cf62215
Ina Tsetsova <inatsetsova@gmail.com>
2021-03-29 21:25:12 +0300

    fix linting

--- src/test/server.test.ts
+++ src/test/server.test.ts
@@ -1,18 +1,18 @@
-import app from "../index"
-import supertest from "supertest"
+import app from '../index'
 const request = supertest(app)
+import supertest from 'supertest'
 
-describe("GET / - a simple api endpoint", () => {
-    it("Hello API Request", async () => {
-      const result = await request.get("/")
-      expect(result.text).toEqual("hello")
+describe('GET / - a simple api endpoint', () => {
+    it('Hello API Request', async () => {
+      const result = await request.get('/')
+      expect(result.text).toEqual('hello')
       expect(result.status).toEqual(200)
     })
   })
 
-  describe("GET /non-exisent", () => {
-    it("Returns a 404", async () => {
-      const result = await request.get("/non-existent")
-      expect(result.status).toEqual(404)
+describe('GET /non-exisent', () => {
+    it('Returns a 404', async () => {
+        const result = await request.get('/non-existent')
+        expect(result.status).toEqual(404)
     })
-  })
+})
