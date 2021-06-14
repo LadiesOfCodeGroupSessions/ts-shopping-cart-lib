9b1c772d2c4eb69bee7a6f278d6eaf1a544ec9cf
Sarah Usher <sarahnusher@gmail.com>
2021-06-14 18:35:00 +0100

    second attempt failed

--- src/test/server.test.ts
+++ src/test/server.test.ts
@@ -1,23 +1,25 @@
 /* tslint:disable */
 import app from '../index'
+import app2 from '../index'
 import supertest from 'supertest'
 
-let request = supertest(app)
+//let request = supertest(app)
+let server = supertest.agent(app);
 
 describe('all server tests', () =>{
-    let server;
-    beforeEach(function() {
-        delete require.cache[require.resolve('../server')];
-        server = require('../server');
-    });
-
-    afterEach(function(done) {
-        server.close(done);
-    });
+    // let server;
+    // beforeEach(function() {
+    //     delete require.cache[require.resolve('../server')];
+    //     server = require('../server');
+    // });
+    //
+    // afterEach(function(done) {
+    //     server.close(done);
+    // });
 
     describe('GET / - a simple api endpoint', () => {
         it('Hello API Request', async () => {
-            const result = await request.get('/')
+            const result = await server.get('/')
             expect(result.text).toEqual('hello')
             expect(result.status).toEqual(200)
         })
@@ -25,56 +27,98 @@ describe('all server tests', () =>{
 
     describe('GET /non-exisent', () => {
         it('Returns a 404', async () => {
-            const result = await request.get('/non-existent')
+            const result = await server.get('/non-existent')
             expect(result.status).toEqual(404)
         })
     })
 
     describe('GET /items', () => {
         it('returns items', async () => {
-            const result = await request.get('/items')
+            const result = await server.get('/items')
             expect(result.status).toEqual(200)
             expect(result.body.items[0].name).toEqual('apples')
         })
     })
 
-    describe('PUT /cart', () => {
-        it('returns cart', async () => {
-            const result = await
-                request.put('/cart')
-                    .send({'id': "123",
-                        'name': 'apple',
-                        'quantity': 1
-                    })
-                    .set('Accept', 'application/json');
+    // test('add numbers async', done => {
+    //     addAsync(10, 5, result => {
+    //         expect(result).toBe(15);
+    //         done();
+    //     })
+    // })
 
-            expect(result.status).toEqual(200)
-            expect(result.body.cart.items[0].quantity).toEqual(1)
-            expect(result.body.cart.id).toEqual("123")
-        })
-    })
+    // describe('PUT /cart', () => {
+    //     it('returns cart',  done => {
+    //         const result =
+    //             server.put('/cart')
+    //                 .send({'id': "123",
+    //                     'name': 'apple',
+    //                     'quantity': 1
+    //                 })
+    //                 .set('Accept', 'application/json')
+    //                 .then((res) =>{
+    //                     // HTTP status should be 200
+    //                     expect(res.status).toEqual(200);
+    //                     expect(res.body.cart.items[0].quantity).toEqual(1);
+    //                     expect(res.body.cart.id).toEqual("123");
+    //                     done()
+    //                 });
+    //
+    //         // request
+    //         //     .get("/")
+    //         //     .expect("Content-Type", /json/)
+    //         //     .expect(200)
+    //         //     .then((response) => {                          // must be then, not a callback
+    //         //         assert(response.body.data !== undefined);
+    //         //     })
+    //         //     .catch((err) => {
+    //         //         assert(err === undefined);
+    //         //     });
+    //
+    //
+    //         //
+    //         // expect(result.status).toEqual(200)
+    //         // expect(result.body.cart.items[0].quantity).toEqual(1)
+    //         // expect(result.body.cart.id).toEqual("123")
+    //     })
+    // })
 
     describe('PUT /cart updates', () => {
-        it('updates existing cart', async () => {
-            await request.put('/cart')
+        let server2 = supertest.agent(app2);
+        it('updates existing cart', done => {
+            server2.put('/cart')
                 .send({'id': "123",
                     'name': 'apple',
                     'quantity': 1
                 })
-                .set('Accept', 'application/json');
+                .set('Accept', 'application/json')
+                .then((res) =>{
+                    // HTTP status should be 200
+                    expect(res.status).toEqual(200);
+                    // expect(res.body.cart.items[0].quantity).toEqual(1);
+                    // expect(res.body.cart.id).toEqual("123");
+                });
 
-            const result = await
-                request.put('/cart')
+
+            server2.put('/cart')
                     .send({'id': "123",
                         'name': 'apple',
                         'quantity': 1
                     })
-                    .set('Accept', 'application/json');
+                    .set('Accept', 'application/json')
+                    .then((res) =>{
+                        // HTTP status should be 200
+                        expect(res.status).toEqual(200);
+                        expect(res.body.cart.items.length).toEqual(1)
+                        expect(res.body.cart.items[0].quantity).toEqual(2)
+                        expect(res.body.cart.id).toEqual("123")
+                        done()
+                    });
 
-            expect(result.status).toEqual(200)
-            expect(result.body.cart.items.length).toEqual(1)
-            expect(result.body.cart.items[0].quantity).toEqual(2)
-            expect(result.body.cart.id).toEqual("123")
+            // expect(result.status).toEqual(200)
+            // expect(result.body.cart.items.length).toEqual(1)
+            // expect(result.body.cart.items[0].quantity).toEqual(2)
+            // expect(result.body.cart.id).toEqual("123")
         })
     })
 
